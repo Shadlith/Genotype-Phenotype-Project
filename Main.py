c1218c5de714319f1635c4714bd2d8effde71d42
Shadlith <sxd980@case.edu>
2025-05-27 10:49:20 -0500

    Initial import
    A quick draft of importing the csv files into arrays, we should probably pop these into their own methods and automate things.

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
+import csv
+
 print("Initial Commit to check this is working")
+
+pheonotypes = []
+with open('Data Samples\phenotypes_202503091344.csv', 'r', encoding="utf8") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        pheonotypes.append(row)
+
+genotypes = []
+with open('Data Samples\\user1_file9_yearofbirth_1985_sex_XY.23andme.txt', 'r', encoding="utf8") as file:
+    reader = csv.reader(file, delimiter='\t')
+    for row in reader:
+        # Skip header lines
+        if row[0].startswith("#"):
+            continue
+        # Append genotype data to the list
+        if len(row) > 1:
+            genotypes.append(row)
+        
+##print("Phenotypes:", pheonotypes)
+print("Genotypes:", genotypes[0:3])
