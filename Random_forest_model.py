b2c4321a0d386e33e12a791f6c02b355c43d11da
Zarita Lee Hetheru <58991148+zhetheru@users.noreply.github.com>
2025-07-23 15:39:35 -0700

    Update Random_forest_model.py

--- Random_forest_model.py
+++ Random_forest_model.py
@@ -1,1 +1,16 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier # Being used for classification
+from sklearn.ensemble import RandomForestRegressor # Being used for regression
+from sklearn.metrics import accuracy_score, mean_squared_error
+
+# Load OpenSNP into the Dataframe
+#dataOpenSNP = pd.read_csv('OpenSNP.csv') blocked out until the data is available and file name can be entered
+
+#Display the first few rows to inspect the data
+##print(dataOpenSNP.head()) blocked out until the data is available and file name can be entered
+
+
+
+
+
